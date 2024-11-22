7aa29978d54e934f7209edbc928bf25899c0b37d
Lark <lark@lark.gay>
2024-11-22 06:39:16 -0500

    Set up logging and CORS in backend

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,9 +1,32 @@
-use axum::{routing::get, Router};
+use axum::http::{
+    header::{HeaderName, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+use axum::{body::Bytes, extract::Path, http::StatusCode, routing::put, Router};
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
 use tower_service::Service;
 use worker::*;
 
+const CORS_ALLOWED_ORIGINS: [&str; 1] = ["https://example.com"];
+const CORS_ALLOWED_METHODS: [Method; 3] = [Method::GET, Method::PUT, Method::DELETE];
+const CORS_ALLOWED_HEADERS: [HeaderName; 1] = [CONTENT_TYPE];
+
 fn router() -> Router {
-    Router::new().route("/", get(root))
+    let cors_layer = CorsLayer::new()
+        .allow_methods(CORS_ALLOWED_METHODS)
+        .allow_headers(CORS_ALLOWED_HEADERS);
+
+    let cors_layer = CORS_ALLOWED_ORIGINS
+        .iter()
+        .fold(cors_layer, |cors_layer, origin| {
+            cors_layer.allow_origin(origin.parse::<HeaderValue>().unwrap())
+        });
+
+    Router::new()
+        .route("/submissions/:id", put(store_form_response))
+        .layer(TraceLayer::new_for_http())
+        .layer(cors_layer)
 }
 
 #[event(fetch)]
@@ -16,6 +39,6 @@ async fn fetch(
     Ok(router().call(req).await?)
 }
 
-pub async fn root() -> &'static str {
-    "Hello Axum!"
+pub async fn store_form_response(Path(id): Path<String>, body: Bytes) -> StatusCode {
+    StatusCode::CREATED
 }
