2148e13dd0f5b5d39fdd9a770b585e15215073ef
Lark <lark@lark.gay>
2024-11-23 05:50:09 -0500

    Define D1 db schema

---
+++ worker/migrations/0001_schema.sql
@@ -0,0 +1,13 @@
+-- Migration number: 0001 	 2024-11-23T10:42:55.874Z
+CREATE TABLE "forms" (
+  "id" integer PRIMARY KEY,
+  "form_id" text NOT NULL UNIQUE,
+  "template" text NOT NULL
+);
+
+CREATE TABLE "submissions" (
+  "id" integer PRIMARY KEY,
+  "form" integer REFERENCES "forms" ("id"),
+  "submission_id" text NOT NULL UNIQUE,
+  "encrypted_body" text NOT NULL
+);
