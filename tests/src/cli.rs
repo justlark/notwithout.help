273261ad43a4a87237f862943cafd231f72f6148
Lark <lark@lark.gay>
2024-11-30 08:46:12 -0500

    Add helper command for challenge responses

--- tests/src/cli.rs
+++ tests/src/cli.rs
@@ -11,12 +11,30 @@ pub struct Cli {
 
 #[derive(Args, Debug, Clone)]
 pub struct GenSigningKey {
+    /// The path of the PEM-formatted private key to generate.
     #[arg(short, long, default_value = "./key.pem")]
     pub output: PathBuf,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct RespondChallenge {
+    /// The challenge token received from the server.
+    ///
+    /// Pass `-` to read from stdin.
+    pub token: String,
+
+    /// The path of the PEM-formatted private key to use for signing the nonce.
+    #[arg(short, long, default_value = "./key.pem")]
+    pub key: PathBuf,
 }
 
 #[derive(Subcommand, Debug, Clone)]
 pub enum Commands {
     /// Generate a new private signing key.
+    ///
+    /// This prints the base64-encoded public key to stdout.
     GenSigningKey(GenSigningKey),
+
+    /// Generate a valid response to an authentication challenge.
+    RespondChallenge(RespondChallenge),
 }
--- tests/src/command.rs
+++ tests/src/command.rs
@@ -1,10 +1,16 @@
-use crate::cli::{Cli, Commands, GenSigningKey};
+use std::io::{self, Read};
+use std::str;
 
+use crate::cli::{Cli, Commands, GenSigningKey, RespondChallenge};
+
+use anyhow::bail;
 use base64::prelude::*;
 use ed25519_dalek::{
     self as ed25519,
-    pkcs8::{spki::der::pem::LineEnding, EncodePrivateKey},
+    pkcs8::{spki::der::pem::LineEnding, DecodePrivateKey, EncodePrivateKey},
+    Signer,
 };
+use serde::{Deserialize, Serialize};
 
 impl GenSigningKey {
     pub fn run(&self) -> anyhow::Result<()> {
@@ -19,10 +25,62 @@ impl GenSigningKey {
     }
 }
 
+#[derive(Debug, Deserialize)]
+struct ApiChallenge {
+    pub nonce: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ApiChallengeResponse {
+    pub signature: String,
+    pub challenge: String,
+}
+
+impl RespondChallenge {
+    pub fn run(&self) -> anyhow::Result<()> {
+        let key = ed25519::SigningKey::read_pkcs8_pem_file(&self.key)?;
+
+        let encoded_challenge = if self.token == "-" {
+            let mut stdin = String::new();
+            io::stdin().read_to_string(&mut stdin)?;
+            stdin
+        } else {
+            self.token.clone()
+        };
+
+        let nonce = match encoded_challenge
+            .splitn(3, '.')
+            .collect::<Vec<_>>()
+            .as_slice()
+        {
+            [_, payload, _] => {
+                let decoded = BASE64_STANDARD.decode(payload)?;
+                let encoded_nonce = serde_json::from_slice::<ApiChallenge>(&decoded)?.nonce;
+                BASE64_STANDARD.decode(encoded_nonce)?
+            }
+            _ => {
+                bail!("Challenge token is not in the expected format.");
+            }
+        };
+
+        let nonce_signature = BASE64_STANDARD.encode(key.sign(&nonce).to_bytes());
+
+        let response = ApiChallengeResponse {
+            signature: nonce_signature,
+            challenge: encoded_challenge,
+        };
+
+        println!("{}", serde_json::to_string_pretty(&response)?);
+
+        Ok(())
+    }
+}
+
 impl Cli {
     pub fn dispatch(&self) -> anyhow::Result<()> {
         match &self.command {
             Commands::GenSigningKey(cmd) => cmd.run(),
+            Commands::RespondChallenge(cmd) => cmd.run(),
         }
     }
 }
