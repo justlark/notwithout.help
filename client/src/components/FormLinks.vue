4aa2786dc6707d336be81016a589b7372b0420a6
Lark <lark@lark.gay>
2024-12-03 18:54:27 -0500

    Refactor form link cards into a separate component

---
+++ client/src/components/FormLinks.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import LinkAdmonition from "@/components/LinkAdmonition.vue";
+import type { ClientKeyId, FormId, SecretLinkKey } from "@/types";
+import { useToast } from "primevue/usetoast";
+import { TOAST_TTL } from "@/vars";
+import Toast from "primevue/toast";
+
+const TOAST_GROUP = "form-link-copy";
+
+const props = defineProps<{
+  formId: FormId;
+  clientKeyId: ClientKeyId;
+  secretLinkKey: SecretLinkKey;
+}>();
+
+const origin = computed(() => window.location.origin);
+const shareLink = computed(() => new URL(`${origin.value}/share/#/${props.formId}`));
+const secretLink = computed(
+  () =>
+    new URL(`${origin.value}/view/#/${props.formId}/${props.clientKeyId}/${props.secretLinkKey}`),
+);
+
+const toast = useToast();
+
+const copyShareLink = () => {
+  toast.removeGroup(TOAST_GROUP);
+
+  toast.add({
+    severity: "info",
+    summary: "Link copied",
+    detail: "Share this link to collect responses.",
+    life: TOAST_TTL,
+    group: TOAST_GROUP,
+  });
+};
+
+const copySecretLink = () => {
+  toast.removeGroup(TOAST_GROUP);
+
+  toast.add({
+    severity: "warn",
+    summary: "Secret link copied",
+    detail: "Be careful who you share this link with.",
+    life: TOAST_TTL,
+    group: TOAST_GROUP,
+  });
+};
+</script>
+
+<template>
+  <div class="max-w-xl mx-auto flex flex-col gap-8">
+    <Toast position="bottom-center" :group="TOAST_GROUP" />
+    <LinkAdmonition :link="shareLink" @click="copyShareLink">
+      <template #title>
+        <span class="flex gap-3 items-center">
+          <i class="pi pi-share-alt"></i>
+          <strong>Share this link</strong>
+        </span>
+      </template>
+
+      <template #details>
+        <ul>
+          <li>Send this link to anyone you want to fill out your survey.</li>
+          <li>
+            People with this link can respond to your survey, but cannot view other responses.
+          </li>
+          <li>
+            Copy this link down in a safe place, because it will disappear when you leave this page.
+          </li>
+        </ul>
+      </template>
+    </LinkAdmonition>
+
+    <LinkAdmonition :link="secretLink" @click="copySecretLink">
+      <template #title>
+        <span class="flex gap-3 items-center">
+          <i class="pi pi-lock"></i>
+          <strong>Keep this link secret</strong>
+        </span>
+      </template>
+
+      <template #details>
+        <ul>
+          <li>Use this link to view responses to your survey.</li>
+          <li>
+            This link is like a password. <strong>Anyone</strong> with this link can access your
+            survey responses.
+          </li>
+          <li>
+            Copy this link down in a safe place, because it will disappear when you leave this page.
+          </li>
+        </ul>
+      </template>
+    </LinkAdmonition>
+  </div>
+</template>
+
+<style scoped></style>
--- client/src/views/OrganizeView.vue
+++ client/src/views/OrganizeView.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import LinkAdmonition from "@/components/LinkAdmonition.vue";
+import { ref } from "vue";
+import FormLinks from "@/components/FormLinks.vue";
 import type { ClientKeyId, FormId, SecretLinkKey } from "@/types";
-import { useToast } from "primevue/usetoast";
-import { TOAST_TTL } from "@/vars";
-import Toast from "primevue/toast";
 
 // TODO: Example of a Form ID.
 const formId = ref("VmrfdKsn" as FormId);
@@ -14,91 +11,12 @@ const clientKeyId = ref("0" as ClientKeyId);
 
 // TODO: Example of a base64url-encoded Secret Link Key.
 const secretLinkKey = ref("6IKYnsjFRUkvZfpcLjlaMEAvo61IUO44y8EIcCbV2rM=" as SecretLinkKey);
-
-const origin = computed(() => window.location.origin);
-const shareLink = computed(() => new URL(`${origin.value}/share/#/${formId.value}`));
-const secretLink = computed(
-  () =>
-    new URL(`${origin.value}/view/#/${formId.value}/${clientKeyId.value}/${secretLinkKey.value}`),
-);
-
-const toast = useToast();
-
-const copyShareLink = () => {
-  toast.removeGroup("link-copy");
-
-  toast.add({
-    severity: "info",
-    summary: "Link copied",
-    detail: "Share this link to collect responses.",
-    life: TOAST_TTL,
-    group: "link-copy",
-  });
-};
-
-const copySecretLink = () => {
-  toast.removeGroup("link-copy");
-
-  toast.add({
-    severity: "warn",
-    summary: "Secret link copied",
-    detail: "Be careful who you share this link with.",
-    life: TOAST_TTL,
-    group: "link-copy",
-  });
-};
 </script>
 
 <template>
-  <Toast position="bottom-center" group="link-copy" />
   <main aria-labelledby="main-heading">
     <h1 id="main-heading" class="text-center mb-10">Organize a group</h1>
-    <div class="max-w-xl mx-auto flex flex-col gap-8">
-      <LinkAdmonition :link="shareLink" @click="copyShareLink">
-        <template #title>
-          <span class="flex gap-3 items-center">
-            <i class="pi pi-share-alt"></i>
-            <strong>Share this link</strong>
-          </span>
-        </template>
-
-        <template #details>
-          <ul>
-            <li>Send this link to anyone you want to fill out your survey.</li>
-            <li>
-              People with this link can respond to your survey, but cannot view other responses.
-            </li>
-            <li>
-              Copy this link down in a safe place, because it will disappear when you leave this
-              page.
-            </li>
-          </ul>
-        </template>
-      </LinkAdmonition>
-
-      <LinkAdmonition :link="secretLink" @click="copySecretLink">
-        <template #title>
-          <span class="flex gap-3 items-center">
-            <i class="pi pi-lock"></i>
-            <strong>Keep this link secret</strong>
-          </span>
-        </template>
-
-        <template #details>
-          <ul>
-            <li>Use this link to view responses to your survey.</li>
-            <li>
-              This link is like a password. <strong>Anyone</strong> with this link can access your
-              survey responses.
-            </li>
-            <li>
-              Copy this link down in a safe place, because it will disappear when you leave this
-              page.
-            </li>
-          </ul>
-        </template>
-      </LinkAdmonition>
-    </div>
+    <FormLinks :formId="formId" :clientKeyId="clientKeyId" :secretLinkKey="secretLinkKey" />
   </main>
 </template>
 
