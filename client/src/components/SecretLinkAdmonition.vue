6901c78856bbdf2c4b7d1745bca8b088d57e4500
Lark <lark@lark.gay>
2024-12-06 07:53:19 -0500

    Refactor components

---
+++ client/src/components/SecretLinkAdmonition.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import LinkAdmonition from "@/components/LinkAdmonition.vue";
+import type { ClientKeyId, FormId } from "@/types";
+import { useToast } from "primevue/usetoast";
+import { TOAST_TTL } from "@/vars";
+import Toast from "primevue/toast";
+import { type SecretLinkKey } from "@/crypto";
+import { encodeBase64Url } from "@/encoding";
+
+const props = defineProps<{
+  formId: FormId | undefined;
+  clientKeyId: ClientKeyId | undefined;
+  secretLinkKey: SecretLinkKey | undefined;
+  toastGroup: string;
+}>();
+
+const origin = computed(() => window.location.origin);
+
+const secretLink = computed(() => {
+  if (!props.formId || !props.clientKeyId || !props.secretLinkKey) {
+    return undefined;
+  }
+
+  return new URL(
+    `${origin.value}/view/#/${props.formId}/${props.clientKeyId}/${encodeBase64Url(props.secretLinkKey)}`,
+  );
+});
+
+const toast = useToast();
+
+const copySecretLink = () => {
+  toast.removeGroup(props.toastGroup);
+
+  toast.add({
+    severity: "warn",
+    summary: "Secret link copied",
+    detail: "Be careful who you share this link with.",
+    life: TOAST_TTL,
+    group: props.toastGroup,
+  });
+};
+</script>
+
+<template>
+  <div class="max-w-xl mx-auto flex flex-col gap-8">
+    <LinkAdmonition v-if="secretLink" :link="secretLink" @click="copySecretLink">
+      <template #title>
+        <span class="flex gap-3 items-center">
+          <i class="pi pi-lock"></i>
+          <strong>Keep this link secret</strong>
+        </span>
+      </template>
+
+      <template #details>
+        <ul>
+          <li>Use this link to view responses to your survey.</li>
+          <li>
+            This link is like a password. <strong>Anyone</strong> with this link can access your
+            survey responses.
+          </li>
+          <li>
+            Copy this link down in a safe place, because it will disappear when you leave this page.
+          </li>
+        </ul>
+      </template>
+    </LinkAdmonition>
+    <Toast position="bottom-center" :group="props.toastGroup" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ client/src/components/ShareLinkAdmonition.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import LinkAdmonition from "@/components/LinkAdmonition.vue";
+import type { FormId } from "@/types";
+import { useToast } from "primevue/usetoast";
+import { TOAST_TTL } from "@/vars";
+import Toast from "primevue/toast";
+
+const props = defineProps<{
+  formId: FormId | undefined;
+  toastGroup: string;
+}>();
+
+const origin = computed(() => window.location.origin);
+
+const shareLink = computed(() => {
+  if (!props.formId) {
+    return undefined;
+  }
+
+  return new URL(`${origin.value}/share/#/${props.formId}`);
+});
+
+const toast = useToast();
+
+const copyShareLink = () => {
+  toast.removeGroup(props.toastGroup);
+
+  toast.add({
+    severity: "info",
+    summary: "Link copied",
+    detail: "Share this link to collect responses.",
+    life: TOAST_TTL,
+    group: props.toastGroup,
+  });
+};
+</script>
+
+<template>
+  <div class="max-w-xl mx-auto flex flex-col gap-8">
+    <LinkAdmonition v-if="shareLink" :link="shareLink" @click="copyShareLink">
+      <template #title>
+        <span class="flex gap-3 items-center">
+          <i class="pi pi-share-alt"></i>
+          <strong>Share this link</strong>
+        </span>
+      </template>
+
+      <template #details>
+        <ul>
+          <li>Send this link to anyone you want to fill out your survey.</li>
+          <li>
+            People with this link can respond to your survey, but cannot view other responses.
+          </li>
+          <li>
+            Copy this link down in a safe place, because it will disappear when you leave this page.
+          </li>
+        </ul>
+      </template>
+    </LinkAdmonition>
+    <Toast position="bottom-center" :group="props.toastGroup" />
+  </div>
+</template>
+
+<style scoped></style>
