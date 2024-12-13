089006fb3e316175f62be25efbcfc9bdf758e7af
Lark <lark@lark.gay>
2024-12-12 19:09:32 -0500

    Add icon to site header

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,14 +3,18 @@ import { RouterView } from "vue-router";
 import Toast from "primevue/toast";
 import { randomTitleLead } from "./vars";
 import { computed } from "vue";
+import icon from "./assets/icon.svg";
 
 const titleLead = computed(() => randomTitleLead());
 </script>
 
 <template>
-  <header class="flex flex-col gap-2 mb-10 justify-baseline items-center text-center">
-    <span class="text-muted-color">{{ titleLead }}</span>
-    <RouterLink to="/" class="text-lg font-medium">Not Without Help</RouterLink>
+  <header class="flex gap-3 items-center mb-8">
+    <img :src="icon" alt="A lifebuoy" class="w-14 h-14" />
+    <div class="flex flex-col gap-1">
+      <span class="text-muted-color">{{ titleLead }}</span>
+      <RouterLink to="/" class="text-lg font-medium">Not Without Help</RouterLink>
+    </div>
   </header>
 
   <RouterView />
