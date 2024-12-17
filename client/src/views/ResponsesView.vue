7de92bbc8395800d75a1e9a35d829a74ba543ff8
Lark <lark@lark.gay>
2024-12-17 07:14:47 -0500

    Add lint ignore rule

--- client/src/views/ResponsesView.vue
+++ client/src/views/ResponsesView.vue
@@ -15,6 +15,7 @@ import { unsealSubmissionBody } from "@/crypto";
 import api, { ApiError, type SubmissionBody } from "@/api";
 import { useConfirm, useToast } from "primevue";
 import { returnsError, isDone, allDone } from "@/types";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useRouter, RouterLink } from "vue-router";
 import useSecretLink from "@/composables/useSecretLink";
 import useAccessToken from "@/composables/useAccessToken";
