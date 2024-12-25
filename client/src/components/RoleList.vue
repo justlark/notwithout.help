05e8d7f27f9ae64d9fa188bf3879252459192237
Lark <lark@lark.gay>
2024-12-25 08:42:50 -0500

    Make roles list more legible on mobile

--- client/src/components/RoleList.vue
+++ client/src/components/RoleList.vue
@@ -18,22 +18,29 @@ const selectedRoleIds = defineModel<Array<string>>();
 
 <template>
   <div>
-    <div v-for="role of props.roles" :key="role.id" class="flex gap-4">
-      <Checkbox
-        v-model="selectedRoleIds"
-        :input-id="`role-checkbox-${role.id}`"
-        :value="role.id"
-        size="large"
-        v-bind="props.inputAttrs"
-      />
-      <div class="flex flex-col gap-2">
-        <label :for="`role-checkbox-${role.id}`" class="font-bold">{{ role.name }}</label>
-        <ul>
-          <li v-for="(responsibility, index) of role.responsibilities" :key="index">
-            {{ responsibility }}
-          </li>
-        </ul>
+    <div v-for="role of props.roles" :key="role.id" class="flex flex-col gap-2">
+      <div class="flex gap-4">
+        <Checkbox
+          v-model="selectedRoleIds"
+          :input-id="`role-checkbox-${role.id}`"
+          :value="role.id"
+          size="large"
+          v-bind="props.inputAttrs"
+        />
+        <div class="flex flex-col gap-2">
+          <label :for="`role-checkbox-${role.id}`" class="font-bold">{{ role.name }}</label>
+          <ul class="hidden sm:block">
+            <li v-for="(responsibility, index) of role.responsibilities" :key="index">
+              {{ responsibility }}
+            </li>
+          </ul>
+        </div>
       </div>
+      <ul class="block sm:hidden">
+        <li v-for="(responsibility, index) of role.responsibilities" :key="index">
+          {{ responsibility }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
